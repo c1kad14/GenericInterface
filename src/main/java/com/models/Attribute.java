a60a6202171c23fe7976baeb09c0b31b491bf27d
c1kad14 <iurii.bobrykov@gmail.com>
2018-03-24 23:36:06 +0200

    init commit

---
+++ src/main/java/com/models/Attribute.java
@@ -0,0 +1,31 @@
+package com.models;
+
+public class Attribute {
+	private Node node;
+	private String name;
+	private String value;
+
+	public Node getNode() {
+		return node;
+	}
+
+	public void setNode(Node nodeName) {
+		this.node = node;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getValue() {
+		return value;
+	}
+
+	public void setValue(String value) {
+		this.value = value;
+	}
+}
